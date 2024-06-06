2c50b4877cf9318681561257fc9cdd3ee8b4ec0d
KymaiselHunter <kyle.bautista78@myhunter.cuny.edu>
2024-06-05 21:25:17 -0400

    learned some basics of functions

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("fuck you")
+//functions tests
+
+const square = function(x)
+{
+    return x*x;
+};
+
+console.log("square of 4 is", square(4));
+
+//let x = square(9);
+//console.log('square of 9 is {x}')
+
+function sayHi()
+{
+    console.log("Hiiiiii!");
+};
+
+sayHi();
